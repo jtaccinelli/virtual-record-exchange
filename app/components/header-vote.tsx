91493779407e723280b85f2d3f6f68a62c49ea4b
Jordan <jtaccinelli@gmail.com>
2024-10-31 13:45:23 +1000

    fix: added back to home page link

--- app/components/header-vote.tsx
+++ app/components/header-vote.tsx
@@ -1,4 +1,7 @@
 import { useMemo } from "react";
+import { Link } from "@remix-run/react";
+import { ArrowLeftIcon } from "@heroicons/react/16/solid";
+
 import { votes } from "context/database";
 import { SpotifyImage } from "./spotify-image";
 
@@ -25,10 +28,14 @@ export function HeaderVote({ playlist, contributors, votes }: Props) {
   }, [contributors, votes]);
 
   return (
-    <div className="flex w-full flex-col p-6 pt-20">
-      <p className="text text-gray-400">Playlist Voting Form</p>
-      <h3 className="heading mb-4">{playlist.name}</h3>
-      <p className="text mb-2 text-gray-400">
+    <div className="flex w-full flex-col gap-6 p-6 pt-10">
+      <Link to="/" className="flex items-center gap-2">
+        <ArrowLeftIcon className="size-4" />
+        <span className="link">Back to home page</span>
+      </Link>
+      <p className="text -mb-6 text-gray-400">Playlist Voting Form</p>
+      <h3 className="heading">{playlist.name}</h3>
+      <p className="text -mb-4 text-gray-400">
         {voted.length} of {contributors.length} votes submitted
       </p>
       <div className="flex items-center gap-1 overflow-x-scroll">
