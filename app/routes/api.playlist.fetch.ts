964ca956d171b872877b11807afeffd5bf76652c
Jordan <jtaccinelli@gmail.com>
2024-11-03 00:17:30 +1000

    fix: simplified playlist search

--- app/routes/api.playlist.fetch.ts
+++ app/routes/api.playlist.fetch.ts
@@ -1,36 +1,27 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { config } from "config";
 import { eq } from "drizzle-orm";
+
+import { fetchPlaylist } from "@app/utils/data";
+
+const FALLBACK_VALUE = {
+  playlist: undefined,
+  hasConfig: false,
+};
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const playlistId = url.searchParams.get("id");
-  if (!playlistId) {
-    return {
-      playlist: undefined,
-      hasConfig: false,
-    };
-  }
+  if (!playlistId) return FALLBACK_VALUE;
 
-  type Response = SpotifyApi.PlaylistObjectFull;
-  const playlist = await context.spotify.fetch<Response>(
-    config.spotify.endpoints.playlists + `/${playlistId}`,
-  );
+  const playlist = await fetchPlaylist(context, playlistId);
+  if (!playlist?.id) return FALLBACK_VALUE;
 
-  if (!playlist) {
-    return {
-      playlist: undefined,
-      hasConfig: false,
-    };
-  }
-
-  const formConfig = await context.db.orm
-    .select()
-    .from(context.db.configs)
-    .where(eq(context.db.configs.playlist_id, playlist.id));
+  const config = await context.db.select(context.db.configs, {
+    where: (table) => eq(table.playlist_id, playlist.id),
+  });
 
   return {
     playlist,
-    hasConfig: formConfig.length > 0,
+    hasConfig: config.length > 0,
   };
 }
